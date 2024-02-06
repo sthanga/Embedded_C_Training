7648431d7c1fbe6e1e45b28e8acc27f27ce0e1ee
S.Thanga <aksthanga@gmail.com>
2024-02-06 22:33:44 +0530

    Create interviewcode.c

---
+++ interviewcode.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+int main()
+{
+    int n = 0b11110000;
+    printf("%d\n",n);
+
+    // Print the fibonacci series : concept
+
+
+
+
+    return 0;
+
+}
